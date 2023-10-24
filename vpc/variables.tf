a45bf308a48e0f98d177517655ccec0dac58ff2b
talhaSl <tmuhammad104@gmail.com>
2023-10-24 18:41:22 +0500

    mainAndVar

---
+++ vpc/variables.tf
@@ -0,0 +1,16 @@
+#enviroment variable 
+variable "region" {}
+variable "project_name" {}
+variable "environment" {}
+
+#vpc variables
+variable "vpc_cidr" {}
+variable "public_subnet_az1_cidr" {}
+variable "public_subnet_az2_cidr" {}
+variable "private_app_subnet_az1_cidr" {}
+variable "private_app_subnet_az2_cidr" {}
+variable "private_data_subnet_az1_cidr" {}
+variable "private_data_subnet_az2_cidr" {}
+
+
+
